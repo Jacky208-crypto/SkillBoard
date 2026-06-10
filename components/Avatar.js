8413795078e24116b071afa7ff9f754042e24dae
Jacky Fnounou <jackyfnounou@gmail.com>
2026-06-09 21:43:06 -0500

    Avatar

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,16 +1,27 @@
 "use client";
- 
+
 import { initials } from "@/lib/helpers";
- 
+
 export default function Avatar({ name, photoUrl, className = "" }) {
   if (photoUrl) {
     return (
-      <span className={`avatar avatar-photo ${className}`}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={photoUrl} alt={name || "avatar"} />
+      <span
+        className={`avatar avatar-photo ${className}`}
+        style={{ overflow: "hidden", padding: 0 }}
+      >
+        <img
+          src={photoUrl}
+          alt={name || "avatar"}
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            display: "block",
+            borderRadius: "50%",
+          }}
+        />
       </span>
     );
   }
   return <span className={`avatar ${className}`}>{initials(name)}</span>;
 }
- 
